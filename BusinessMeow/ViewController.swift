b5a7c09da2ad3949445a3c381773d64f4321f06b
Steve Gravrock <sdg@panix.com>
2024-03-13 20:26:32 -0700

    Play meow of disapproval

--- BusinessMeow/ViewController.swift
+++ BusinessMeow/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    
+    private var soundId: SystemSoundID = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // didBecomeActiveNotification will trigger on initial launch,
+        // so we don't also need to call playMeowOfDisapproval directly.
+        NotificationCenter.default.addObserver(self, selector: #selector(playMeowOfDisapproval), name: UIApplication.didBecomeActiveNotification, object: nil)
     }
-
+    
+    @objc func playMeowOfDisapproval() {
+        if soundId == 0 {
+            guard let soundURL = Bundle.main.url(forResource: "Meow_domestic_cat", withExtension: "mp3") else {
+                print("Audio file not found")
+                return
+            }
+            
+            let ret: OSStatus = AudioServicesCreateSystemSoundID(soundURL as CFURL, &soundId)
+    
+            if ret != 0 {
+                print("Loading audio failed with code", ret);
+                return;
+            }
+        }
+        
+        AudioServicesPlaySystemSound(soundId);
+    }
 
 }
 
